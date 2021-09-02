259c87a43aa9c143e1fc2dae67eed614fbe1c1e9
github-AKZhuk <lesha.zhuk@gmail.com>
2021-09-02 11:54:59 +0300

    impement: defautl interface for state

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,12 +1,12 @@
-import { IAction, IState } from '../types';
+import { IAction, IState1 } from '../types';
 import { SWITCH_IS_LOAD } from './actions';
 
-const defaultState: IState = {
+const defaultState: IState1 = {
   isLoad: false,
   error: '',
 };
 
-export const reducer = (state = defaultState, action: IAction): IState => {
+export const reducer = (state = defaultState, action: IAction): IState1 => {
   switch (action.type) {
     case SWITCH_IS_LOAD:
       return { ...state, isLoad: !state.isLoad };
--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,37 @@ export interface IAction {
   payload?: null | string;
 }
 
-export interface IState {
+export interface IState1 {
   isLoad: boolean;
   error: string;
+}
+
+enum GameRole {
+  scrumMaster = 'scrum master',
+  player = 'player',
+  observer = 'observer',
+}
+interface IState {
+  user: IUser;
+  memders: IUser[];
+  issues: { title: string; priority: string; link: string }[];
+  gameSettings: IGameSettings;
+}
+
+interface IUser {
+  firstName: string;
+  lastName?: string;
+  jobPostion?: string;
+  urlToImage: string;
+  role: typeof GameRole;
+}
+
+interface IGameSettings {
+  isScrumMasterAsPlayer: boolean;
+  changingCardInRoundEnd: boolean;
+  isTimerNeeded: boolean;
+  scoreType: string;
+  scorTypeShort: string;
+  timer?: string;
+  cards: { value: string; name: string }[];
 }
