6a2545322f0bbb6611d040d2d13540e7dffd27df
alexeyvalko <valko.alexey@gmail.com>
2021-09-03 17:31:33 +0300

    feat-3: fix upload button

--- src/components/shared/UploadButton.tsx
+++ src/components/shared/UploadButton.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,15 +23,9 @@ const UploadButton = (): JSX.Element => {
     <div className={classes.root}>
       <label htmlFor="contained-button-file">
         <input accept="image/*" className={classes.input} id="contained-button-file" multiple type="file" />
-        <Button variant="contained" color="primary" component="span">
+        <Button variant="contained" color="primary" component="span" startIcon={<CloudUploadIcon />}>
           Upload
         </Button>
-      </label>
-      <label htmlFor="icon-button-file">
-        <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
-        <IconButton color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
       </label>
     </div>
   );
