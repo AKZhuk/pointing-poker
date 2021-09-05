e0ee2300ecdb755d749a617a66098d90016c6d53
Aliaksei <lesha.zhuk@gmail.com>
2021-09-05 12:53:41 +0300

    Revert "Feature 4"

--- src/redux/reducers/connection/connectionActions.ts
+++
@@ -1,8 +0,0 @@
-import { IAction, IConnection } from '../../../types';
-
-export const SET_CONNECTION = 'SET_CONNECTION';
-
-export const setConnection = (title: keyof IConnection, value: string): IAction => ({
-  type: SET_CONNECTION,
-  payload: { [title]: value },
-});
--- src/redux/reducers/issues/issuesActions.ts
+++
@@ -1,8 +0,0 @@
-import { IIssue, IIssueAction } from '../../../types';
-
-export const DELETE_ISSUE = 'DELETE_ISSUE';
-
-export const removeIssue = (issue: IIssue): IIssueAction => ({
-  type: DELETE_ISSUE,
-  payload: issue,
-});
