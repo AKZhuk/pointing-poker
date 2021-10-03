cf1516691db6a268bdb2b24c9788ca125dd20908
Aliaksei Zhuk <lesha.zhuk@gmail.com>
2021-10-03 23:44:01 +0300

    feat:implement img pre-loader

--- src/components/shared/GameCards/GameCard.tsx
+++ src/components/shared/GameCards/GameCard.tsx
@@ -1,4 +1,6 @@
 import { Card } from '@material-ui/core';
+import { Img } from 'react-image';
+import { Skeleton } from '@material-ui/lab';
 import { useSelector } from 'react-redux';
 import ReactCardFlip from 'react-card-flip';
 import { IRootState } from '../../../types';
@@ -19,9 +21,14 @@ const GameCard = ({
   return (
     <ReactCardFlip isFlipped={isFlip} flipDirection="horizontal" flipSpeedBackToFront={1} flipSpeedFrontToBack={1}>
       <Card elevation={8} className={large ? 'game-card_large game-card_front' : 'game-card game-card_front'}>
-        <img className="game-card__img" src={`./assets/${scoreType}/${value}.png`} alt={`card ${value}`} />
+        <Img
+          className="game-card__img"
+          src={`./assets/d${scoreType}/${value}.png`}
+          loader={<Skeleton variant="rect" height="100%" animation="wave" />}
+          unloader={<Skeleton variant="rect" height="100%" animation="wave" />}
+          alt={`card ${value}`}
+        />
       </Card>
-
       <Card elevation={8} className={large ? 'game-card_large' : 'game-card'}>
         <img className="game-card__img" src={cardBack} alt="card back img" />
       </Card>
